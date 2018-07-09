3642056778809ecf158596d246d4e6ab7a568217
lvoyee <lvoyee@163.com>
2018-07-09 21:21:03 +0800

    update: update pom files

--- src/main/java/com/yicheng/test/DemoTest.java
+++ src/main/java/com/yicheng/test/DemoTest.java
@@ -10,9 +10,49 @@ import org.springframework.test.context.junit4.SpringRunner;
  *
  * @author luo.yicheng
  */
-@RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+/*@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)*/
 public class DemoTest {
 
+
+    public static void main(String[] args) {
+
+        String str ="Mozilla/5.0 (iPad; CPU OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1";
+
+
+
+
+        System.out.print(isPcMacOs(str));
+
+    }
+
+    /**
+     *
+     * 判断是否为 PC 中的 MAC
+     * @param ua UA
+     * @return TRUE FALSE
+     */
+    public static boolean isPcMacOs(String ua){
+
+        ua = ua.toLowerCase();
+        boolean falg = false;
+
+        try {
+            ua = ua.substring(ua.indexOf("like"),ua.indexOf("x")+1);
+
+            System.out.println(ua);
+
+            if (ua.equals("like mac os x")){
+                falg = false;
+            }
+
+
+        }catch (Exception e){
+            falg = true;
+        }
+
+        return falg;
+
+    }
 
 }
